87e0fbe9725bb6101c0ff66940473678d131262b
mikleo04 <hafidsiraj.aw@gmail.com>
2025-01-18 08:47:39 +0700

    feature: make plugin and register in the server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import Hapi from '@hapi/hapi';
-import routes from './routes.js';
 import process from 'process';
+import notes from './api/notes';
+import NotesService from './service/inMemory/notesService.js';
 
 const init =  async () => {
+  const notesService = new NotesService();
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0', // in the production environment can not use localhost, 0.0.0.0 can allow all the network interfaces
@@ -14,7 +17,12 @@ const init =  async () => {
     }
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server running on ${server.info.uri}`);
